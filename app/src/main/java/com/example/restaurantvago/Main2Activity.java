9c7e4a489e1e71459c3f2833bed98017f6547be5
Amrykid <amrykid@gmail.com>
2019-04-22 22:15:01 -0500

    Added sub-menus.

--- app/src/main/java/com/example/restaurantvago/Main2Activity.java
+++ app/src/main/java/com/example/restaurantvago/Main2Activity.java
@@ -80,12 +80,14 @@ public class Main2Activity extends AppCompatActivity  {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case R.id.action_map:
+                return true;
+            case R.id.action_about_us:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
